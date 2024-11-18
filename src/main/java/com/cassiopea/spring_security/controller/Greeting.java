4fb09ae6aa1c9827ac1e391f50747b08ca590810
Serigne Saliou WADE <serignesaliou.wade@intouchgroup.net>
2024-11-18 16:45:09 +0000

    Role based authorization

--- src/main/java/com/cassiopea/spring_security/controller/Greeting.java
+++ src/main/java/com/cassiopea/spring_security/controller/Greeting.java
@@ -1,5 +1,6 @@
 package com.cassiopea.spring_security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,19 @@ public class Greeting {
     @GetMapping
     public  String hello () {
         return "There we go Cassiopea!" ;
+    }
+
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user" )
+    public String userEndpoint () {
+        return "User landing page!" ;
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin" )
+    public String adminDashboard () {
+        return "ADMIN dashboard page !!s!" ;
     }
 }
