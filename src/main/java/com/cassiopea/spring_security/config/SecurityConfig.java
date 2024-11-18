358f83e158fda89b9ca56bb7fe4c5072c570a3ac
Serigne Saliou WADE <serignesaliou.wade@intouchgroup.net>
2024-11-18 16:26:24 +0000

    In memory user details management

--- src/main/java/com/cassiopea/spring_security/config/SecurityConfig.java
+++ src/main/java/com/cassiopea/spring_security/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,8 +18,26 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain (HttpSecurity http ) throws Exception {
         http.authorizeHttpRequests ( requests -> requests.anyRequest().authenticated() ) ;
+        http.formLogin ( Customizer.withDefaults() ) ;
         http.httpBasic ( Customizer.withDefaults() ) ;
 
-        return http.build () ; 
+        return http.build () ;
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+
+        UserDetails user1 = User.withUsername ( "user1" )
+                .password ( "{noop}user1" )
+                .roles ( "USER" )
+                .build() ;
+
+        UserDetails admin1 = User.withUsername ( "admin1" )
+                .password ( "{noop}admin1" )
+                .roles ( "ADMIN" )
+                .build () ;
+
+        return new InMemoryUserDetailsManager( user1 , admin1 ) ;
     }
 }
