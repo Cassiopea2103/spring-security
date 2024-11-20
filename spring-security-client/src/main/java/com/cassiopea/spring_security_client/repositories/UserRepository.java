b1ef93cfeb89335ab9c94f3c13ead48b5e0fcfbd
Serigne Saliou WADE <serignesaliou.wade@intouchgroup.net>
2024-11-20 09:47:01 +0000

    Thymeleaf app added

--- spring-security-client/src/main/java/com/cassiopea/spring_security_client/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cassiopea.spring_security_client.repositories;
-
-import com.cassiopea.spring_security_client.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
